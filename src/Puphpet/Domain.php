0e617fe4bf9959e34512d04f823e90289c0371d4
Juan Treminio <jtreminio@gmail.com>
2013-05-14 23:37:40 -0500

    Separated out explode and quote methods

--- src/Puphpet/Domain.php
+++ src/Puphpet/Domain.php
@@ -4,19 +4,28 @@ namespace Puphpet;
 
 class Domain
 {
-    protected function explodeAndQuote($values)
+    /**
+     * @param mixed $values
+     * @return array
+     */
+    public function explodeAndQuote($values)
     {
         $values = !empty($values) ? explode(',', $values) : [];
 
-        $values = array_map(
-            function($value)
-            {
+        return $this->quoteArray($values);
+    }
+
+    /**
+     * @param $values
+     * @return array
+     */
+    protected function quoteArray($values)
+    {
+        return array_map(function($value) {
                 $value = str_replace("'", '', str_replace('"', '', $value));
                 return "'{$value}'";
             },
             $values
         );
-
-        return $values;
     }
 }
