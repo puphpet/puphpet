493f9e24b187b77eecb3283dd6a43566c944cf4c
Juan Treminio <jtreminio@gmail.com>
2013-05-17 18:01:14 -0500

    Domain\PuppetModule\MySQL fully covered

---
+++ tests/Puphpet/Tests/Domain/PuppetModule/MySQLTest.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace Puphpet\Tests\Domain\PuppetModule;
+
+use Puphpet\Domain\PuppetModule\MySQL;
+use Puphpet\Tests\Base;
+
+class MySQLTest extends Base
+{
+    protected $mysqlArray = array();
+
+    public function setUp()
+    {
+        $this->mysqlArray = [
+            'root'   => 'rootPassword',
+            'dbuser' => [
+                [
+                    'privileges' => [
+                        'ALL',
+                    ],
+                    'user'     => 'user1',
+                    'password' => 'password1',
+                    'dbname'   => 'dbname1',
+                    'host'     => 'localhost',
+                ],
+                [
+                    'privileges' => [
+                        'ALTER',
+                        'CREATE',
+                        'CREATE TEMPORARY TABLES',
+                    ],
+                    'user'     => 'user2',
+                    'password' => 'password2',
+                    'dbname'   => 'dbname2',
+                    'host'     => 'remotehost',
+                ],
+                [
+                    'privileges' => [
+                        'CREATE',
+                    ],
+                    'user'     => 'user3',
+                    'password' => 'password3',
+                    'dbname'   => 'dbname3',
+                    'host'     => 'amazon',
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider providerGetFormattedReturnsEmptyArrayWhenMySQLPropertyEmpty
+     */
+    public function testGetFormattedReturnsEmptyArrayWhenMySQLPropertyEmpty($param)
+    {
+        $mysql = new MySQL($param);
+
+        $expected = array();
+
+        $this->assertEquals(
+            $expected,
+            $mysql->getFormatted()
+        );
+    }
+
+    public function providerGetFormattedReturnsEmptyArrayWhenMySQLPropertyEmpty()
+    {
+        return [
+            [array()],
+            [0],
+            [''],
+        ];
+    }
+
+    public function testGetFormattedRemovesIncompleteSubmissions()
+    {
+        $expected = $this->mysqlArray;
+
+        unset($expected['dbuser'][1], $expected['dbuser'][2]);
+
+        unset($this->mysqlArray['dbuser'][1]['user']);
+
+        unset($this->mysqlArray['dbuser'][2]['dbname']);
+
+        $mysql = new MySQL($this->mysqlArray);
+
+        $this->assertEquals(
+            $expected,
+            $mysql->getFormatted()
+        );
+    }
+
+    public function testGetFormattedReturnsArrayWithEmptyKeyIfNoDbuserValues()
+    {
+        $this->mysqlArray['dbuser'] = false;
+
+        $expected = [
+            'root'   => 'rootPassword',
+            'dbuser' => array()
+        ];
+
+        $mysql = new MySQL($this->mysqlArray);
+
+        $this->assertEquals(
+            $expected,
+            $mysql->getFormatted()
+        );
+    }
+}
