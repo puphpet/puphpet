f876787a858580d48de8c516a5330562e7de1d73
Juan Treminio <jtreminio@gmail.com>
2013-08-16 21:45:59 -0500

    v2 - Initial versions

---
+++ src/Puphpet/Domain/Module/Register.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Puphpet\Domain\Module;
+
+use Puphpet\Domain;
+
+class Register implements Domain\RegisterInterface
+{
+    public function __construct(array $data = [])
+    {
+        //
+    }
+
+    public function register()
+    {
+        //
+    }
+
+    public function sanitize()
+    {
+        //
+    }
+
+    public function process()
+    {
+        //
+    }
+}
---
+++ src/Puphpet/Domain/Server/Register.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Puphpet\Domain\Server;
+
+use Puphpet\Domain;
+
+class Register implements Domain\RegisterInterface
+{
+    public function __construct(array $data = [])
+    {
+        //
+    }
+
+    public function register()
+    {
+        //
+    }
+
+    public function sanitize()
+    {
+        //
+    }
+
+    public function process()
+    {
+        //
+    }
+}
