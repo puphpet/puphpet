e720c17cced985dcd994f13e45ca2a6c15d3c3ff
Juan Treminio <jtreminio@gmail.com>
2013-09-03 21:17:55 -0500

    v2 - Vagrantfile bundle

---
+++ src/Puphpet/Extension/VagrantfileBundle/Controller/DefaultController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Puphpet\Extension\VagrantfileBundle\Controller;
+
+use Puphpet\MainBundle\Extension;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Yaml\Yaml;
+
+class DefaultController extends Controller implements Extension\ControllerInterface
+{
+    public function indexAction(array $data = [])
+    {
+        if (empty($data)) {
+            $data = $this->getData();
+        }
+
+        return $this->render('PuphpetExtensionVagrantfileBundle:form:Vagrantfile.html.twig', [
+            'vagrantfile' => $data,
+        ]);
+    }
+
+    public function syncedFolderAction()
+    {
+        return $this->render('PuphpetExtensionVagrantfileBundle:form/sections:SyncedFolder.html.twig', [
+            'synced_folder' => array_shift($this->getData()['vm']['synced_folder']),
+        ]);
+    }
+
+    public function forwardedPortAction()
+    {
+        return $this->render('PuphpetExtensionVagrantfileBundle:form/sections:ForwardedPort.html.twig', [
+            'forwarded_port' => array_shift($this->getData()['vm']['network']['forwarded_port']),
+        ]);
+    }
+
+    /**
+     * @return array
+     */
+    private function getData()
+    {
+        return Yaml::parse(__DIR__ . '/../Resources/config/data.yml');
+    }
+}
---
+++ src/Puphpet/Extension/VagrantfileBundle/PuphpetExtensionVagrantfileBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Puphpet\Extension\VagrantfileBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class PuphpetExtensionVagrantfileBundle extends Bundle
+{
+}
