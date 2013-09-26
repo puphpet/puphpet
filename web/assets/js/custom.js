4e593c2478c8b49f72d27689d95a1205a9414030
Juan Treminio <jtreminio@gmail.com>
2013-09-25 21:46:03 -0500

    v2 - Made selectize work with dynamically added elements

--- web/assets/js/custom.js
+++ web/assets/js/custom.js
@@ -8,8 +8,36 @@ $(document).ready(function() {
         $('#vagrantfile-vm-box').val(vmBox);
     });
 
-    // input or select elements; allows user to create their own tags
-    $('.tags, .select-tags-editable').selectize({
+    runSelectize(null);
+
+    // add repeatable containers based on button data source-url
+    $(document).on('click', 'button.addParentContainer', function(e){
+        var sourceUrl = this.getAttribute('data-source-url');
+        var buttonEle = $(this);
+
+        $.ajax({
+            url: sourceUrl,
+            cache: false
+        }).done(function(response) {
+            var $row = $(response).insertBefore(buttonEle.closest('.row'));
+            runSelectize($row);
+        });
+
+    });
+
+    // delete repeatable containers based on button data id
+    $(document).on('click', 'button.deleteParentContainer', function(e){
+        var parentId = this.getAttribute('data-parent-id');
+        var $parentContainer = $('#' + parentId);
+
+        $parentContainer.remove();
+
+    });
+});
+
+function runSelectize($element) {
+        // input or select elements; allows user to create their own tags
+    $('.tags, .select-tags-editable', $element).selectize({
         plugins: ['remove_button'],
         delimiter: ',',
         persist: false,
@@ -22,7 +50,7 @@ $(document).ready(function() {
     });
 
     // select elements; asks user for value of selected tags; cannot create own tags
-    $('.select-tags-user-input').selectize({
+    $('.select-tags-user-input', $element).selectize({
         plugins: ['remove_button'],
         delimiter: ',',
         persist: false,
@@ -39,33 +67,10 @@ $(document).ready(function() {
     });
 
     // select elements; does not allow creating new tags
-    $('.select-tags').selectize({
+    $('.select-tags', $element).selectize({
         plugins: ['remove_button'],
         delimiter: ',',
         persist: false,
         create: false
     });
-
-    // add repeatable containers based on button data source-url
-    $(document).on('click', 'button.addParentContainer', function(e){
-        var sourceUrl = this.getAttribute('data-source-url');
-        var buttonEle = $(this);
-
-        $.ajax({
-            url: sourceUrl,
-            cache: false
-        }).done(function(response) {
-            $(response).insertBefore(buttonEle.closest('.row'));
-        });
-
-    });
-
-    // delete repeatable containers based on button data id
-    $(document).on('click', 'button.deleteParentContainer', function(e){
-        var parentId = this.getAttribute('data-parent-id');
-        var $parentContainer = $('#' + parentId);
-
-        $parentContainer.remove();
-
-    });
-});
+}
