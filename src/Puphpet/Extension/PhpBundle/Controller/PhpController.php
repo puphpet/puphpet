0b3a94c7160d0a87e99dc9f2cec11d4cc3b67cc0
Juan Treminio <jtreminio@gmail.com>
2013-09-26 23:31:12 -0500

    v2 - Upgrading Php extension to newer style

--- src/Puphpet/Extension/PhpBundle/Controller/PhpController.php
+++ src/Puphpet/Extension/PhpBundle/Controller/PhpController.php
@@ -5,26 +5,13 @@ namespace Puphpet\Extension\PhpBundle\Controller;
 use Puphpet\MainBundle\Extension;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\Yaml\Yaml;
 
 class PhpController extends Controller implements Extension\ControllerInterface
 {
-    public function indexAction(array $data = [])
+    public function indexAction(array $data)
     {
-        if (empty($data)) {
-            $data = $this->getData();
-        }
-
         return $this->render('PuphpetExtensionPhpBundle:form:Php.html.twig', [
             'php' => $data,
         ]);
-    }
-
-    /**
-     * @return array
-     */
-    private function getData()
-    {
-        return Yaml::parse(__DIR__ . '/../Resources/config/data.yml');
     }
 }
