50ea15ced98a5532b3467ee94b65580b2d22deca
Juan Treminio <jtreminio@gmail.com>
2013-09-26 23:02:11 -0500

    v2 - Upgrading ServerBasics extension to newer style

--- src/Puphpet/Extension/ServerBasicsBundle/Controller/ServerBasicsController.php
+++ src/Puphpet/Extension/ServerBasicsBundle/Controller/ServerBasicsController.php
@@ -5,26 +5,13 @@ namespace Puphpet\Extension\ServerBasicsBundle\Controller;
 use Puphpet\MainBundle\Extension;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\Yaml\Yaml;
 
 class ServerBasicsController extends Controller implements Extension\ControllerInterface
 {
-    public function indexAction(array $data = [])
+    public function indexAction(array $data)
     {
-        if (empty($data)) {
-            $data = $this->getData();
-        }
-
         return $this->render('PuphpetExtensionServerBasicsBundle:form:ServerBasics.html.twig', [
             'server' => $data,
         ]);
-    }
-
-    /**
-     * @return array
-     */
-    private function getData()
-    {
-        return Yaml::parse(__DIR__ . '/../Resources/config/data.yml');
     }
 }
