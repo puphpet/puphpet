8c5b543405b5d3c6d4698f8b1c832160eda3afaf
Juan Treminio <jtreminio@gmail.com>
2013-10-06 14:42:16 -0500

    v2 - -quiet => --quiet; auto-answer yes

--- src/Puphpet/MainBundle/Resources/views/manifest/shell/update-puppet.sh
+++ src/Puphpet/MainBundle/Resources/views/manifest/shell/update-puppet.sh
@@ -1,6 +1,6 @@
 if [[ ! -f /puppet-updated ]]; then
-    wget -quiet -O /tmp/puppet.deb http://apt.puppetlabs.com/puppetlabs-release-"$1".deb
+    wget --quiet -O /tmp/puppet.deb http://apt.puppetlabs.com/puppetlabs-release-"$1".deb
     dpkg -i /tmp/puppet.deb
-    apt-get update && apt-get install puppet
+    apt-get update && apt-get -y install puppet
     touch /puppet-updated
 fi
