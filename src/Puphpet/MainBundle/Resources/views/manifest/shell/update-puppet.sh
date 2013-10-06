602919e1201985816e058080bf5097e554364244
Juan Treminio <jtreminio@gmail.com>
2013-10-06 13:46:55 -0500

    v2 - Adding "--parser=future" and fixing bad char

--- src/Puphpet/MainBundle/Resources/views/manifest/shell/update-puppet.sh
+++ src/Puphpet/MainBundle/Resources/views/manifest/shell/update-puppet.sh
@@ -1,5 +1,5 @@
 if [[ ! -f /puppet-updated ]]; then
-    wget –quiet -O /tmp/puppet.deb http://apt.puppetlabs.com/puppetlabs-release-"$1".deb
+    wget -quiet -O /tmp/puppet.deb http://apt.puppetlabs.com/puppetlabs-release-"$1".deb
     dpkg -i /tmp/puppet.deb
     apt-get update && apt-get install puppet
     touch /puppet-updated
