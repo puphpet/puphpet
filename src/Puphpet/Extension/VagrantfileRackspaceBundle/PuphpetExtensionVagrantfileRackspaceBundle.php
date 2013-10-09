45242c7f988c06edf10efaadf9a8986ec5a5b6b0
Juan Treminio <jtreminio@gmail.com>
2013-10-08 17:59:39 -0700

    v2 - Adding missing files

---
+++ src/Puphpet/Extension/VagrantfileRackspaceBundle/PuphpetExtensionVagrantfileRackspaceBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Puphpet\Extension\VagrantfileRackspaceBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class PuphpetExtensionVagrantfileRackspaceBundle extends Bundle
+{
+}
