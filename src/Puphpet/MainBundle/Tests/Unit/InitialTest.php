3107c6f01dfd894f17a88d6127f44a60e34f68e4
Juan Treminio <jtreminio@gmail.com>
2013-10-12 22:34:02 -0500

    Initial unit testing skeleton

---
+++ src/Puphpet/MainBundle/Tests/Unit/InitialTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Puphpet\Extension\FooBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DefaultControllerTest extends WebTestCase
+{
+    public function testSimple()
+    {
+        $this->assertTrue(true);
+    }
+}
