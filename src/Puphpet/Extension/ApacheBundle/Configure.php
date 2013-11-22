0178bfe1b675c5a241d11678a262a4906e105de3
Frank Stelzer <dev@frankstelzer.de>
2013-11-22 15:56:36 +0000

    [#75] added very basic ssl support for apache

--- src/Puphpet/Extension/ApacheBundle/Configure.php
+++ src/Puphpet/Extension/ApacheBundle/Configure.php
@@ -35,4 +35,61 @@ class Configure extends Extension\ExtensionAbstract
     {
         return $this->container->get('puphpet.extension.apache.manifest_controller');
     }
+
+    /**
+     * @inheritdoc
+     */
+    public function getData()
+    {
+        $data = parent::getData();
+        $data = $this->fixVhosts($data);
+
+        return $data;
+    }
+
+    /**
+     * @param array $data
+     *
+     * @return array
+     */
+    protected function fixVhosts(array $data)
+    {
+        foreach ($data['vhosts'] as $id => $vhost) {
+            $ssl = $vhost['ssl'];
+
+            // "http" vhost should be doubled
+            if ($ssl === '1') {
+                // reset original vhost
+                $data['vhosts'][$id]['ssl'] = false;
+
+                // clone vhost and add ssl version
+                $sslVhost = $this->generateSslVhost($vhost);
+                $sslId = $id . '_ssl';
+
+                // add ssl vhost only if it does not exist already
+                if (!array_key_exists($sslId, $data)) {
+                    $data['vhosts'][$sslId] = $sslVhost;
+                }
+            } elseif ($ssl === '2') { // ssl only
+                $data['vhosts'][$id]['ssl'] = true;
+            } else {
+                $data['vhosts'][$id]['ssl'] = false;
+            }
+        }
+
+        return $data;
+    }
+
+    /**
+     * @param array $sslVhost
+     *
+     * @return array
+     */
+    protected function generateSslVhost(array $sslVhost)
+    {
+        $sslVhost['port'] = 443;
+        $sslVhost['ssl'] = true;
+
+        return $sslVhost;
+    }
 }
