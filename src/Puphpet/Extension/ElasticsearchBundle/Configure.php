931ceffe7251d3bb74338b00805da2dac8e6656d
Jason Endres <jaendres@gmail.com>
2014-01-09 20:38:02 -0600

    configuring elastic search to show up

--- src/Puphpet/Extension/ElasticsearchBundle/Configure.php
+++ src/Puphpet/Extension/ElasticsearchBundle/Configure.php
@@ -10,7 +10,7 @@ class Configure extends Extension\ExtensionAbstract
 {
     protected $name = 'Elasticsearch';
     protected $slug = 'elasticsearch';
-    protected $targetFile = 'puppet/manifests/default.pp';
+    protected $targetFile = 'puphpet/puppet/manifests/default.pp';
 
     protected $sources = [
         'mysql' => ":git => 'https://github.com/elasticsearch/puppet-elasticsearch'",
