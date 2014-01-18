74a99a82a68e2267f4fcfee8f1c36c1e37440e14
unknown <podarokua@gmail.com>
2014-01-18 13:15:39 +0200

    Initial Varnish bundle created

---
+++ src/Puphpet/Extension/VarnishBundle/Configure.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Puphpet\Extension\VarnishBundle;
+
+use Puphpet\MainBundle\Extension;
+
+use Symfony\Component\DependencyInjection\ContainerInterface as Container;
+
+class Configure extends Extension\ExtensionAbstract
+{
+    protected $name = 'Varnish';
+    protected $slug = 'varnish';
+    protected $targetFile = 'puphpet/puppet/manifest.pp';
+
+    protected $sources = [
+        'puppet-varnish'     => ":git => 'https://github.com/podarok/puppet-varnish.git'",
+    ];
+
+    /**
+     * @param Container $container
+     */
+    public function __construct(Container $container)
+    {
+        $this->dataLocation = __DIR__ . '/Resources/config';
+
+        parent::__construct($container);
+    }
+
+    public function getFrontController()
+    {
+        return $this->container->get('puphpet.extension.varnish.front_controller');
+    }
+
+    public function getManifestController()
+    {
+        return $this->container->get('puphpet.extension.varnish.manifest_controller');
+    }
+}
