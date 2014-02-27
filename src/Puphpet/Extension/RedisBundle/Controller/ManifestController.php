dd133cbdf971e76a4c9c32ddaa9b19ee57fa9584
Ankur Paul <paul.ankur@gmail.com>
2014-02-27 19:59:02 +1100

    Added more redis stuff

---
+++ src/Puphpet/Extension/RedisBundle/Controller/ManifestController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Puphpet\Extension\RedisBundle\Controller;
+
+use Puphpet\MainBundle\Extension;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class ManifestController extends Controller implements Extension\ControllerInterface
+{
+    public function indexAction(array $data)
+    {
+        return $this->render('PuphpetExtensionRedisBundle:manifest:Redis.pp.twig', [
+            'data' => $data,
+        ]);
+    }
+}
