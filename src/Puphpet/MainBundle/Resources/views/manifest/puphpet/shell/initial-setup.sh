033a87f5e617a04d25c7f092f7a386de522e2a0f
Elijah Madden <elijah.madden@nikkoam.com>
2014-03-07 14:29:29 +0900

    Update Ruby the right way.

--- src/Puphpet/MainBundle/Resources/views/manifest/puphpet/shell/initial-setup.sh
+++ src/Puphpet/MainBundle/Resources/views/manifest/puphpet/shell/initial-setup.sh
@@ -29,7 +29,16 @@ if [[ ! -f /.puphpet-stuff/initial-setup-repo-update ]]; then
         echo "Updating to Ruby 1.9.3"
         yum install centos-release-SCL >/dev/null
         yum remove ruby >/dev/null
-        yum install ruby193 facter hiera ruby193-ruby-irb ruby193-ruby-doc ruby193-rubygem-json ruby193-libyaml >/dev/null
+
+        # As described here http://tecadmin.net/install-ruby-1-9-3-or-multiple-ruby-verson-on-centos-6-3-using-rvm/
+        yum install gcc-c++ patch readline readline-devel zlib zlib-devel >/dev/null
+        yum install libyaml-devel libffi-devel openssl-devel make >/dev/null
+        yum install bzip2 autoconf automake libtool bison iconv-devel >/dev/null
+        curl -L get.rvm.io | bash -s stable
+        source /etc/profile.d/rvm.sh
+        rvm install 1.9.3
+        rvm use 1.9.3 --default
+
         gem update --system >/dev/null
         gem install haml >/dev/null
         echo "Finished updating to Ruby 1.9.3"
