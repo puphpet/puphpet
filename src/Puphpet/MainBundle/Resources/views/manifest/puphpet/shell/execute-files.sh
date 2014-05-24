b239a33f655502b50acc3f5a4429a2f825824f20
nakanaa <miiojanis@gmail.com>
2014-05-24 03:18:58 +0300

    Update execute-files.sh
    Run exec-once files that have not been run before. Closes #764

--- src/Puphpet/MainBundle/Resources/views/manifest/puphpet/shell/execute-files.sh
+++ src/Puphpet/MainBundle/Resources/views/manifest/puphpet/shell/execute-files.sh
@@ -7,12 +7,16 @@ VAGRANT_CORE_FOLDER=$(cat '/.puphpet-stuff/vagrant-core-folder.txt')
 shopt -s nullglob
 files=("${VAGRANT_CORE_FOLDER}"/files/exec-once/*)
 
-if [[ ! -f '/.puphpet-stuff/exec-once-ran' && (${#files[@]} -gt 0) ]]; then
+if [[ (${#files[@]} -gt 0) ]]; then
     echo 'Running files in files/exec-once'
-    find "${VAGRANT_CORE_FOLDER}/files/exec-once" -maxdepth 1 -not -path '*/\.*' -type f \( ! -iname "empty" \) -exec chmod +x '{}' \; -exec {} \;
+    if [ ! -d '/.puphpet-stuff/exec-once-ran' ]; then 
+       mkdir '/.puphpet-stuff/exec-once-ran'
+       echo 'Created directory /.puphpet-stuff/exec-once-ran'
+    fi
+    find "${VAGRANT_CORE_FOLDER}/files/exec-once" -maxdepth 1 -not -path '*/\.*' -type f \( ! -iname "empty" \) -exec cp -n '{}' /.puphpet-stuff/exec-once-ran \;
+    find /.puphpet-stuff/exec-once-ran -maxdepth 1 -type f -exec chmod +x '{}' \; -exec {} \; -exec sh -c '>{}' \;
     echo 'Finished running files in files/exec-once'
-    echo 'To run again, delete file /.puphpet-stuff/exec-once-ran'
-    touch /.puphpet-stuff/exec-once-ran
+    echo 'To run again, delete file(s) you want rerun in /.puphpet-stuff/exec-once-ran or the whole folder to rerun all'
 fi
 
 echo 'Running files in files/exec-always'
