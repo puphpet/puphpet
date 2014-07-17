ac47ecd32e3f2c04b0c0f4c7c0a2f9ed8fd9075e
Juan Treminio <jtreminio@gmail.com>
2014-07-17 02:05:03 -0500

    Symlinks gem

--- src/Puphpet/MainBundle/Resources/views/manifest/puphpet/shell/install-ruby.sh
+++ src/Puphpet/MainBundle/Resources/views/manifest/puphpet/shell/install-ruby.sh
@@ -16,8 +16,13 @@ echo 'Installing Ruby 1.9.3 using RVM'
 
 curl -sSL https://get.rvm.io | bash -s stable --ruby=1.9.3
 source /usr/local/rvm/scripts/rvm
+
 mv /usr/bin/ruby /usr/bin/ruby-old
+mv /usr/bin/gem /usr/bin/gem-old
+
 ln -s /usr/local/rvm/rubies/ruby-1.9.3-*/bin/ruby /usr/bin/ruby
+ln -s /usr/local/rvm/rubies/ruby-1.9.3-*/bin/gem /usr/bin/gem
+
 gem update --system >/dev/null
 
 touch '/.puphpet-stuff/install-ruby'
