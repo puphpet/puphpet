e06dbfb9c296b4fe6e0df671400842ed269285fa
Juan Treminio <jtreminio@gmail.com>
2014-07-17 01:42:01 -0500

    Sets proper location for helper scripts

--- src/Puphpet/MainBundle/Resources/views/manifest/puphpet/shell/install-ruby.sh
+++ src/Puphpet/MainBundle/Resources/views/manifest/puphpet/shell/install-ruby.sh
@@ -2,9 +2,10 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
-VAGRANT_CORE_FOLDER=$(echo "$1")
+VAGRANT_CORE_FOLDER=$(cat '/.puphpet-stuff/vagrant-core-folder.txt')
 
 OS=$(/bin/bash "${VAGRANT_CORE_FOLDER}/shell/os-detect.sh" ID)
+RELEASE=$(/bin/bash "${VAGRANT_CORE_FOLDER}/shell/os-detect.sh" RELEASE)
 CODENAME=$(/bin/bash "${VAGRANT_CORE_FOLDER}/shell/os-detect.sh" CODENAME)
 
 if [[ -f '/.puphpet-stuff/install-ruby' ]]; then
