08c6d41a5e4582d7b31b5977b4ece2486891f5af
Juan Treminio <jtreminio@gmail.com>
2014-08-06 21:34:14 -0500

    Fixes double slashes in url "//" to single slash "/"

--- web/app.php
+++ web/app.php
@@ -4,6 +4,12 @@ use Symfony\Component\HttpFoundation\Request;
 
 umask(0000); // This will let the permissions be 0777
 
+foreach (['SCRIPT_URL', 'REQUEST_URI', 'SCRIPT_NAME', 'ORIG_SCRIPT_FILENAME', 'PATH_TRANSLATED', 'PHP_SELF'] as $key) {
+    if (!empty($_SERVER[$key])) {
+        $_SERVER[$key] = str_replace('//', '/', $_SERVER[$key]);
+    }
+}
+
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 
 $loader->register(true);
--- web/app_dev.php
+++ web/app_dev.php
@@ -13,6 +13,12 @@ ini_set('opcache.enable', '0');
 
 umask(0000);
 
+foreach (['SCRIPT_URL', 'REQUEST_URI', 'SCRIPT_NAME', 'ORIG_SCRIPT_FILENAME', 'PATH_TRANSLATED', 'PHP_SELF'] as $key) {
+    if (!empty($_SERVER[$key])) {
+        $_SERVER[$key] = str_replace('//', '/', $_SERVER[$key]);
+    }
+}
+
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 Debug::enable();
 
