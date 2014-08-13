09836a2030a416f498396304738bfa0f4aa99ebb
shivapoudel <axisthemes@gmail.com>
2014-08-13 06:15:48 +0545

    Generated and modified WPCliBundle

---
+++ src/Puphpet/Extension/WPCliBundle/Controller/FrontController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Puphpet\Extension\WPCliBundle\Controller;
+
+use Puphpet\MainBundle\Extension;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class FrontController extends Controller implements Extension\ControllerInterface
+{
+    public function indexAction(array $data, $extra = '')
+    {
+        return $this->render('PuphpetExtensionWPCliBundle:form:WPCli.html.twig', [
+            'wpcli' => $data,
+        ]);
+    }
+}
---
+++ src/Puphpet/Extension/WPCliBundle/PuphpetExtensionWPCliBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Puphpet\Extension\WPCliBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class PuphpetExtensionWPCliBundle extends Bundle
+{
+}
