581672189af7bf67672561abc65c1bc4fca4fd88
Juan Treminio <jtreminio@gmail.com>
2014-11-27 00:00:51 -0600

    Adds -f to rm for non-existing files

--- src/Puphpet/MainBundle/Resources/views/manifest/puphpet/shell/install-ruby.sh
+++ src/Puphpet/MainBundle/Resources/views/manifest/puphpet/shell/install-ruby.sh
@@ -11,7 +11,7 @@ CODENAME=$(/bin/bash "${VAGRANT_CORE_FOLDER}/shell/os-detect.sh" CODENAME)
 function check_ruby_symlinks() {
     # Ruby or Gem not symlinked
     if [ ! -L '/usr/bin/ruby' ] || [ ! -L '/usr/bin/gem' ]; then
-        rm '/.puphpet-stuff/install-ruby'
+        rm -f '/.puphpet-stuff/install-ruby'
 
         return 0;
     fi
@@ -21,23 +21,23 @@ function check_ruby_symlinks() {
 
     # If ruby symlink is old-style pointing to /usr/local/rvm/wrappers/default/ruby
     if [ "grep '/usr/local/rvm/wrappers/default' ${RUBY_SYMLINK}" ]; then
-        rm '/usr/bin/ruby'
+        rm -f '/usr/bin/ruby'
 
         if [[ -f '/usr/local/rvm/rubies/ruby-1.9.3-p547/bin/ruby' ]]; then
             ln -s '/usr/local/rvm/rubies/ruby-1.9.3-p547/bin/ruby' '/usr/bin/ruby'
         else
-            rm '/.puphpet-stuff/install-ruby'
+            rm -f '/.puphpet-stuff/install-ruby'
         fi
     fi
 
     # If gem symlink is old-style pointing to /usr/local/rvm/wrappers/default/gem
     if [ "grep '/usr/local/rvm/wrappers/default' ${GEM_SYMLINK}" ]; then
-        rm '/usr/bin/gem'
+        rm -f '/usr/bin/gem'
 
         if [[ -f '/usr/local/rvm/rubies/ruby-1.9.3-p547/bin/gem' ]]; then
             ln -s '/usr/local/rvm/rubies/ruby-1.9.3-p547/bin/gem' '/usr/bin/gem'
         else
-            rm '/.puphpet-stuff/install-ruby'
+            rm -f '/.puphpet-stuff/install-ruby'
         fi
     fi
 }
