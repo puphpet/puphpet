2410b49e204982af894d949b49a690e52e33dc87
Juan Treminio <jtreminio@gmail.com>
2014-12-04 10:11:52 -0600

    Manages Java outside of module; Adds versions
    Previously the Java install was being managed by the elasticsearch/elasticsearch
    module. Adding Solr which requires Java also brought in puppetlabs/java
    to manage Java installs, making ES's unecessary.
    
    Also adds version options.
    
    Closes #1241

--- archive/puphpet/puppet/nodes/ElasticSearch.pp
+++ archive/puphpet/puppet/nodes/ElasticSearch.pp
@@ -3,16 +3,25 @@ if $elasticsearch_values == undef { $elasticsearch_values = hiera_hash('elastic_
 include puphpet::params
 
 if hash_key_equals($elasticsearch_values, 'install', 1) {
+  $es_version = $elasticsearch_values['settings']['version']
+
   case $::osfamily {
-    'debian': { $elasticsearch_package_url = 'https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.2.1.deb' }
-    'redhat': { $elasticsearch_package_url = 'https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.2.1.noarch.rpm' }
+    'debian': { $elasticsearch_package_url = "https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-${es_version}.deb" }
+    'redhat': { $elasticsearch_package_url = "https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-${es_version}.noarch.rpm" }
     default:  { fail('Unrecognized operating system for Elastic Search') }
   }
 
-  $elasticsearch_settings = merge($elasticsearch_values['settings'], {
-    'package_url' => $elasticsearch_package_url,
-    require       => Class['my_fw::post'],
-  })
+  if ! defined(Class['java']) and $elasticsearch_values['settings']['java_install'] {
+    class { 'java':
+      distribution => 'jre',
+    }
+  }
+
+  $elasticsearch_settings = delete(merge($elasticsearch_values['settings'], {
+    'java_install' => false,
+    'package_url'  => $elasticsearch_package_url,
+    require        => Class['my_fw::post'],
+  }), 'version')
 
   create_resources('class', { 'elasticsearch' => $elasticsearch_settings })
 }
