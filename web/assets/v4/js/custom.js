puphpet/puphpet

commit 2947f6e8ad8ab257a15f1c96dfc3eed02ba9cee3

Removes console.log()

web/assets/v4/js/custom.js
@@ -846,10 +846,7 @@ PUPHPET.mirrorValue = function() {
         var $target    = $(this);
         var sourceName = $(this)[0].getAttribute('data-mirror-value');
 
-        console.log(sourceName);
-
         $(document).on('change', sourceName, function(e) {
-            console.log('aaaa');
             $target.val($(sourceName).val());
         });
     });

commit 7d8172d8d8b0d71691dab47a6cc1e00b92ceba0a

Disables unchecked tabs; broken by 2947f6e

web/assets/v4/js/custom.js
@@ -497,7 +497,7 @@ PUPHPET.showAvailableOptions = function() {
  * Runs on initial page load
  */
 PUPHPET.disableInactiveTabElements = function() {
-    $('.nav.nav-tabs.icon-bar.single-select li').each(function() {
+    $('.nav.single-select li').each(function() {
         if ($(this).hasClass('active')) {
             return;
         }
@@ -520,7 +520,7 @@ PUPHPET.disableInactiveTabElements = function() {
  * tabs that belong to multi-selectable groups
  */
 PUPHPET.enableClickedTabElement = function() {
-    $('.nav.nav-tabs.icon-bar.single-select li a[data-toggle="tab"]').on('shown.bs.tab', function(e) {
+    $('.nav.single-select li a[data-toggle="tab"]').on('shown.bs.tab', function(e) {
         if ($(this).parent().parent().hasClass('multi-select')) {
             return;
         }