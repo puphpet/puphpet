29f505d9a68d0856d84e233cccd9cde09f09dd6c
Juan Treminio <jtreminio@gmail.com>
2015-03-27 17:39:02 -0500

    Converts VagrantfileLocalBundle to v5 design

--- src/Puphpet/Extension/VagrantfileLocalBundle/Controller/FrontController.php
+++ src/Puphpet/Extension/VagrantfileLocalBundle/Controller/FrontController.php
@@ -17,14 +17,14 @@ class FrontController extends Controller implements Extension\ControllerInterfac
 
     public function syncedFolderAction()
     {
-        return $this->render('PuphpetExtensionVagrantfileLocalBundle:sections:SyncedFolder.html.twig', [
+        return $this->render('PuphpetExtensionVagrantfileLocalBundle:sections:synced-folder.html.twig', [
             'synced_folder' => $this->getData()['empty_synced_folder'],
         ]);
     }
 
     public function forwardedPortAction()
     {
-        return $this->render('PuphpetExtensionVagrantfileLocalBundle:sections:ForwardedPort.html.twig', [
+        return $this->render('PuphpetExtensionVagrantfileLocalBundle:sections:forwarded-port.html.twig', [
             'forwarded_port' => array_shift($this->getData()['vm']['network']['forwarded_port']),
         ]);
     }
