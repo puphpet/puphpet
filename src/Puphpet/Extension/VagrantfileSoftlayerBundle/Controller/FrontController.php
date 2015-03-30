71df11af3012408af49689e4aa41f16784090f44
Juan Treminio <jtreminio@gmail.com>
2015-03-30 09:53:49 -0500

    Converts VagrantfileSoftLayerBundle to v5 design

--- src/Puphpet/Extension/VagrantfileSoftlayerBundle/Controller/FrontController.php
+++ src/Puphpet/Extension/VagrantfileSoftlayerBundle/Controller/FrontController.php
@@ -17,7 +17,7 @@ class FrontController extends Controller implements Extension\ControllerInterfac
 
     public function syncedFolderAction()
     {
-        return $this->render('PuphpetExtensionVagrantfileSoftlayerBundle:sections:SyncedFolder.html.twig', [
+        return $this->render('PuphpetExtensionVagrantfileSoftlayerBundle:sections:synced-folder.html.twig', [
             'synced_folder' => $this->getData()['empty_synced_folder'],
         ]);
     }
