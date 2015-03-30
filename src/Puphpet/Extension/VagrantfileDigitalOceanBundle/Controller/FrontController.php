ba9566caec455308c35fa8f3a5dac5e9696cb58a
Juan Treminio <jtreminio@gmail.com>
2015-03-30 11:33:54 -0500

    Converts VagrantfileDigitalOceanBundle to v5 design

--- src/Puphpet/Extension/VagrantfileDigitalOceanBundle/Controller/FrontController.php
+++ src/Puphpet/Extension/VagrantfileDigitalOceanBundle/Controller/FrontController.php
@@ -17,7 +17,7 @@ class FrontController extends Controller implements Extension\ControllerInterfac
 
     public function syncedFolderAction()
     {
-        return $this->render('PuphpetExtensionVagrantfileDigitalOceanBundle:sections:SyncedFolder.html.twig', [
+        return $this->render('PuphpetExtensionVagrantfileDigitalOceanBundle:sections:synced-folder.html.twig', [
             'synced_folder' => $this->getData()['empty_synced_folder'],
         ]);
     }
