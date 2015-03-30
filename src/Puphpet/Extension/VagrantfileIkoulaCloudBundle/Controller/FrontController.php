3821f3b52383be3bc393eb076f615fe82c6ce723
Juan Treminio <jtreminio@gmail.com>
2015-03-30 11:53:27 -0500

    Converts VagrantfileIkoulaCloudBundle to v5 design

--- src/Puphpet/Extension/VagrantfileIkoulaCloudBundle/Controller/FrontController.php
+++ src/Puphpet/Extension/VagrantfileIkoulaCloudBundle/Controller/FrontController.php
@@ -17,7 +17,7 @@ class FrontController extends Controller implements Extension\ControllerInterfac
 
     public function syncedFolderAction()
     {
-        return $this->render('PuphpetExtensionVagrantfileIkoulaCloudBundle:sections:SyncedFolder.html.twig', [
+        return $this->render('PuphpetExtensionVagrantfileIkoulaCloudBundle:sections:synced-folder.html.twig', [
             'synced_folder' => $this->getData()['empty_synced_folder'],
         ]);
     }
