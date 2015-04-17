f856ad48a18708c751866a7947e22b3a66963877
Juan Treminio <jtreminio@gmail.com>
2015-04-17 11:29:53 -0500

    Disables opcache and bumps up error reporting

--- src/Puphpet/MainBundle/Tests/bootstrap.php
+++ src/Puphpet/MainBundle/Tests/bootstrap.php
@@ -2,3 +2,9 @@
 
 $loader = require __DIR__ . '/../../../../vendor/autoload.php';
 $loader->add('Puphpet\Tests\Unit\MainBundle', __DIR__ . '/Unit');
+
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+ini_set('opcache.enable', '0');
+
+umask(0000);
