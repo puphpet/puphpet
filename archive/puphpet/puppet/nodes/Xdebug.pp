0fce24429ec35df6f6f9f6ec6029b5aba5793657
Juan Treminio <jtreminio@gmail.com>
2015-04-30 01:27:29 -0500

    Explicitly imports all manifests; fixes puphpet::php::settings not parsed before xdebug. Fixes #1585

--- archive/puphpet/puppet/nodes/Xdebug.pp
+++ archive/puphpet/puppet/nodes/Xdebug.pp
@@ -1,7 +1,5 @@
 if $xdebug_values == undef { $xdebug_values = hiera_hash('xdebug', false) }
 if $php_values == undef { $php_values = hiera_hash('php', false) }
-if $apache_values == undef { $apache_values = hiera_hash('apache', false) }
-if $nginx_values == undef { $nginx_values = hiera_hash('nginx', false) }
 
 include puphpet::params
 
@@ -11,23 +9,6 @@ if hash_key_equals($xdebug_values, 'install', 1)
   Class['Puphpet::Php::Settings']
   -> Class['Puphpet::Php::Xdebug']
 
-  $xdebug_php_prefix = $::osfamily ? {
-    'debian' => 'php5-',
-    'redhat' => 'php-',
-  }
-
-  if hash_key_equals($apache_values, 'install', 1)
-    and hash_key_equals($php_values, 'mod_php', 1)
-  {
-    $xdebug_webserver_service = 'httpd'
-  } elsif hash_key_equals($apache_values, 'install', 1)
-    or hash_key_equals($nginx_values, 'install', 1)
-  {
-    $xdebug_webserver_service = "${xdebug_php_prefix}fpm"
-  } else {
-    $xdebug_webserver_service = undef
-  }
-
   $xdebug_compile = $php_values['settings']['version'] ? {
     '5.6'   => true,
     '56'    => true,
@@ -35,7 +16,7 @@ if hash_key_equals($xdebug_values, 'install', 1)
   }
 
   class { 'puphpet::php::xdebug':
-    webserver => $xdebug_webserver_service,
+    webserver => $puphpet::php::settings::service,
     compile   => $xdebug_compile,
   }
 
@@ -44,7 +25,7 @@ if hash_key_equals($xdebug_values, 'install', 1)
       entry       => "XDEBUG/${key}",
       value       => $value,
       php_version => $php_values['settings']['version'],
-      webserver   => $xdebug_webserver_service
+      webserver   => $puphpet::php::settings::service
     }
   }
 }
