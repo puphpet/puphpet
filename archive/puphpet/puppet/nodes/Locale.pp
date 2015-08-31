8e4e65ff0628e81f112f4e8311895df7df3748eb
Juan Treminio <jtreminio@gmail.com>
2015-08-30 22:43:43 -0500

    Adds "UTF-8 to user locales. Fixes #1861

--- archive/puphpet/puppet/nodes/Locale.pp
+++ archive/puphpet/puppet/nodes/Locale.pp
@@ -12,12 +12,12 @@ class puphpet_locale (
 
     $user_locale = array_true($settings, 'locales') ? {
       true    => $settings['locales'],
-      default => ['en_US.UTF-8 UTF-8', 'en_GB.UTF-8 UTF-8']
+      default => ['en_US.UTF-8', 'en_GB.UTF-8']
     }
 
     $merged = merge($settings, {
       'default_locale' => $default_locale,
-      'locales'        => $user_locale,
+      'locales'        => suffix($user_locale, ' UTF-8'),
     })
 
     create_resources('class', { 'locales' => $merged })
