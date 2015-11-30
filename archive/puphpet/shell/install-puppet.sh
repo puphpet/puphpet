d4c71db3a2363d724f36535b7cb48550f19fcb89
Juan Treminio <jtreminio@gmail.com>
2015-11-30 17:37:05 -0600

    Tightens code for debian/centos 6

--- archive/puphpet/shell/install-puppet.sh
+++ archive/puphpet/shell/install-puppet.sh
@@ -8,15 +8,25 @@ OS=$(/bin/bash "${VAGRANT_CORE_FOLDER}/shell/os-detect.sh" ID)
 RELEASE=$(/bin/bash "${VAGRANT_CORE_FOLDER}/shell/os-detect.sh" RELEASE)
 CODENAME=$(/bin/bash "${VAGRANT_CORE_FOLDER}/shell/os-detect.sh" CODENAME)
 
-if [[ ! -f /.puphpet-stuff/install-puppet ]]; then
-    echo 'Installing Puppet'
-    rpm -Uvh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-6.noarch.rpm
-    yum -y install puppet-agent
-
-    rm -f /usr/bin/puppet
-    ln -s /opt/puppetlabs/bin/puppet /usr/bin/puppet
-
-    /opt/puppetlabs/puppet/bin/gem install deep_merge activesupport vine --no-ri --no-rdoc
-
-    echo 'Finished installing Puppet'
+if [[ -f /.puphpet-stuff/install-puppet ]]; then
+    exit 0
 fi
+
+echo 'Installing Puppet'
+
+if [ "${OS}" == 'debian' ] || [ "${OS}" == 'ubuntu' ]; then
+    echo "Debian support coming soon"
+elif [[ "${OS}" == 'centos' ]]; then
+    if [ "${RELEASE}" == 6 ]; then
+        rpm -Uvh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-6.noarch.rpm
+        yum -y install puppet-agent
+
+        rm -f /usr/bin/puppet
+        ln -s /opt/puppetlabs/bin/puppet /usr/bin/puppet
+
+        /opt/puppetlabs/puppet/bin/gem install deep_merge activesupport vine --no-ri --no-rdoc
+    fi
+fi
+
+echo 'Finished installing Puppet'
+touch /.puphpet-stuff/install-puppet
