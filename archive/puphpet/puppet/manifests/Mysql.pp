5ac9098b0ba32ff11e9401cfec2e24c5077dc7d4
Juan Treminio <jtreminio@gmail.com>
2015-12-05 11:02:17 -0600

    Updates php-mysql module name for debian php 7

--- archive/puphpet/puppet/manifests/Mysql.pp
+++ archive/puphpet/puppet/manifests/Mysql.pp
@@ -167,6 +167,8 @@ class puphpet_mysql (
       $php_module = 'mysql'
     } elsif $::lsbdistcodename == 'lucid' or $::lsbdistcodename == 'squeeze' {
       $php_module = 'mysql'
+    } elsif $::osfamily == 'debian' and $php['settings']['version'] in ['7.0', '70'] {
+      $php_module = 'mysql'
     } else {
       $php_module = 'mysqlnd'
     }
