1cf2b5a7050b5807013c42d7fff1d0e16b862b29
Juan Treminio <jtreminio@gmail.com>
2015-12-06 21:09:14 -0600

    Adds version to MongoDB settings; creates pid/log file directories because module does not. Fixes #2024

--- archive/puphpet/puppet/manifests/MongoDb.pp
+++ archive/puphpet/puppet/manifests/MongoDb.pp
@@ -20,9 +20,23 @@ class puphpet_mongodb (
   Class['mongodb::globals']
   -> Class['mongodb::server']
 
-  class { 'mongodb::globals':
-    manage_package_repo => true,
+  # The GUI does not choose a version by default
+  $global_settings = array_true($mongodb, 'global') ? {
+    true    => $mongodb['global'],
+    default => { }
   }
+
+  $merged_globals = merge({
+    'manage_package_repo' => true,
+    'version'             => $::osfamily ? {
+      'debian' => '2.6.11',
+      'redhat' => '2.6.11-1',
+    },
+  }, $global_settings)
+
+  create_resources('class', {
+    'mongodb::globals' => $merged_globals
+  })
 
   create_resources('class', {
     'mongodb::server' => $mongodb['settings']
@@ -32,6 +46,40 @@ class puphpet_mongodb (
     class { 'mongodb::client':
       require => Class['mongodb::server']
     }
+  }
+
+  # MongoDB module does not handle creating log and pidfile directories
+  if ! defined(User[$::mongodb::params::user]) {
+    user { $::mongodb::params::user:
+      ensure     => present,
+      managehome => false,
+    }
+  }
+
+  file { '/var/log/mongodb':
+    ensure  => directory,
+    owner   => $::mongodb::params::user,
+    mode    => '0775',
+    notify  => Service[$::mongodb::globals::service_name],
+    require => User[$::mongodb::params::user]
+  }
+  -> file { '/var/run/mongodb':
+    ensure  => directory,
+    owner   => $::mongodb::params::user,
+    mode    => '0775',
+    notify  => Service[$::mongodb::globals::service_name],
+  }
+  -> file { $::mongodb::params::pidfilepath:
+    ensure  => file,
+    owner   => $::mongodb::params::user,
+    mode    => '0775',
+    notify  => Service[$::mongodb::globals::service_name],
+  }
+  -> file { $::mongodb::params::logpath:
+    ensure  => file,
+    owner   => $::mongodb::params::user,
+    mode    => '0775',
+    notify  => Service[$::mongodb::globals::service_name],
   }
 
   each( $mongodb['databases'] ) |$key, $database| {
