d3fee9e8a5096698822ffc5078d34fb1d82e716d
Juan Treminio <jtreminio@gmail.com>
2015-12-16 10:27:42 -0600

    Installing RVM on system breaks packaged Puppet ruby/gem; force install required gems on provision; fixes #2049

--- archive/puphpet/shell/install-puppet.sh
+++ archive/puphpet/shell/install-puppet.sh
@@ -8,29 +8,27 @@ OS=$(/bin/bash "${VAGRANT_CORE_FOLDER}/shell/os-detect.sh" ID)
 RELEASE=$(/bin/bash "${VAGRANT_CORE_FOLDER}/shell/os-detect.sh" RELEASE)
 CODENAME=$(/bin/bash "${VAGRANT_CORE_FOLDER}/shell/os-detect.sh" CODENAME)
 
-if [[ -f /.puphpet-stuff/install-puppet ]]; then
-    exit 0
-fi
+if [[ ! -f /.puphpet-stuff/install-puppet ]]; then
+    echo 'Installing Puppet'
 
-echo 'Installing Puppet'
-
-if [ "${OS}" == 'debian' ] || [ "${OS}" == 'ubuntu' ]; then
-    URL="https://apt.puppetlabs.com/puppetlabs-release-pc1-${CODENAME}.deb"
-    wget --quiet --tries=5 --connect-timeout=10 -O /.puphpet-stuff/puppetlabs-release-pc1.deb ${URL}
-    dpkg -i /.puphpet-stuff/puppetlabs-release-pc1.deb
-    apt-get update
-    apt-get -y install puppet-agent
-elif [[ "${OS}" == 'centos' ]]; then
-    if [ "${RELEASE}" == 6 ]; then
-        rpm -Uvh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-6.noarch.rpm
-        yum -y install puppet-agent
+    if [ "${OS}" == 'debian' ] || [ "${OS}" == 'ubuntu' ]; then
+        URL="https://apt.puppetlabs.com/puppetlabs-release-pc1-${CODENAME}.deb"
+        wget --quiet --tries=5 --connect-timeout=10 -O /.puphpet-stuff/puppetlabs-release-pc1.deb ${URL}
+        dpkg -i /.puphpet-stuff/puppetlabs-release-pc1.deb
+        apt-get update
+        apt-get -y install puppet-agent
+    elif [[ "${OS}" == 'centos' ]]; then
+        if [ "${RELEASE}" == 6 ]; then
+            rpm -Uvh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-6.noarch.rpm
+            yum -y install puppet-agent
+        fi
     fi
-fi
 
-rm -f /usr/bin/puppet
-ln -s /opt/puppetlabs/bin/puppet /usr/bin/puppet
+    rm -f /usr/bin/puppet
+    ln -s /opt/puppetlabs/bin/puppet /usr/bin/puppet
+
+    echo 'Finished installing Puppet'
+    touch /.puphpet-stuff/install-puppet
+fi
 
 /opt/puppetlabs/puppet/bin/gem install deep_merge activesupport vine --no-ri --no-rdoc
-
-echo 'Finished installing Puppet'
-touch /.puphpet-stuff/install-puppet
