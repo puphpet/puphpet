9361d7ab5d67c83a3f58a7ea11bbe1c0e218ad21
Juan Treminio <jtreminio@gmail.com>
2016-01-22 12:38:07 -0600

    puphpet::php::ini handles ini for pecl. Fixes #2125

--- archive/puphpet/puppet/manifests/Redis.pp
+++ archive/puphpet/puppet/manifests/Redis.pp
@@ -28,18 +28,6 @@ class puphpet_redis (
       service_autorestart => $webserver_restart,
       require             => Class['redis']
     }
-
-    if $::osfamily == 'debian'
-      and $puphpet::php::settings::version in ['54', '5.4']
-    {
-      puphpet::php::ini { 'REDIS/extension':
-        entry       => "REDIS/extension",
-        value       => 'redis.so',
-        php_version => '5.4',
-        webserver   => $puphpet::php::settings::service,
-        notify      => Service[$puphpet::php::settings::service],
-      }
-    }
   }
 
 }
