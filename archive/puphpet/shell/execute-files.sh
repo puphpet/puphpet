bb0f2ecf5d14c7b552f5bb41b3e7d902e5b59e54
Juan Treminio <jtreminio@gmail.com>
2016-02-21 00:12:06 -0600

    Fixes issue with no exec_always_dir passed

--- archive/puphpet/shell/execute-files.sh
+++ archive/puphpet/shell/execute-files.sh
@@ -34,6 +34,10 @@ done
 echo "Finished running files in files/${EXEC_ONCE_DIR}"
 echo "To run again, delete hashes you want rerun in /.puphpet-stuff/${EXEC_ONCE_DIR}-ran or the whole file to rerun all"
 
+if [ -z ${EXEC_ALWAYS_DIR} ]; then
+    exit 0
+fi
+
 echo "Running files in files/${EXEC_ALWAYS_DIR}"
 
 find "${VAGRANT_CORE_FOLDER}/files/${EXEC_ALWAYS_DIR}" -maxdepth 1 -type f -name '*.sh' | sort | while read FILENAME; do
