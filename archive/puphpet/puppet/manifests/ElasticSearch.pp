ecc7c5a4c6836f49f7c8906581cf10171bd1b549
Juan Treminio <jtreminio@gmail.com>
2016-04-12 21:40:03 -0500

    Updates elasticsearch module. Fixes #2239

--- archive/puphpet/puppet/manifests/ElasticSearch.pp
+++ archive/puphpet/puppet/manifests/ElasticSearch.pp
@@ -6,41 +6,16 @@ class puphpet_elasticsearch (
     puphpet::firewall::port { "9200": }
   }
 
-  $settings = $elasticsearch['settings']
-  $version  = $elasticsearch['settings']['version']
-
-  $url_base = 'https://download.elasticsearch.org/elasticsearch/elasticsearch'
-
-  case $::osfamily {
-    'debian': {
-      $url = "${url_base}/elasticsearch-${version}.deb"
-    }
-    'redhat': {
-      # Versions less than 2.0.0 have .noarch on the rpm
-      if $version =~ /^1\./ {
-        $noarch_rpm = '.noarch'
-      } else {
-        $noarch_rpm = ''
-      }
-
-      $url = "${url_base}/elasticsearch-${version}${noarch_rpm}.rpm"
-    }
-    default: {
-      fail('Unrecognized operating system for Elastic Search')
-    }
-  }
-
   if ! defined(Class['java']) and $settings['java_install'] {
     class { 'java':
       distribution => 'jre',
     }
   }
 
-  $merged = delete(merge($settings, {
+  $merged = merge($settings, {
     'java_install' => false,
-    'package_url'  => $url,
     require        => Class['puphpet::firewall::post'],
-  }), 'version')
+  })
 
   create_resources('class', { 'elasticsearch' => $merged })
 
