0d6474bb0b0c25be15360a4945eced83ef648842
Juan Treminio <jtreminio@gmail.com>
2016-06-20 01:58:17 -0500

    Adds puphpet::server::wget

---
+++ archive/puphpet/puppet/manifests/puphpet/manifests/server/wget.pp
@@ -0,0 +1,38 @@
+# == Define Resource Type: puphpet::server::wget
+#
+# Downloads a remote file.
+#
+# Usage:
+#
+# puphpet::server::wget { '/download/to/target':
+#   source => 'http://source.com/foo.exe',
+#   user   => 'user',
+#   group  => 'group',
+#   mode   => '+x'
+# }
+#
+define puphpet::server::wget (
+  $source,
+  $user  = 'root',
+  $group = 'root',
+  $mode
+){
+
+  $destination = $name
+
+  $cmd = "wget --quiet --tries=5 --connect-timeout=10 -O '${destination}' ${source}"
+
+  exec { "download ${source}":
+    creates => $destination,
+    command => $cmd,
+    timeout => 3600,
+    path    => '/usr/bin',
+  }
+  -> file { $destination:
+    ensure => file,
+    owner  => $user,
+    group  => $group,
+    mode   => $mode,
+  }
+
+}
