ab67ec603a4967fc01ef42b2ec2f32f5372f7677
Juan Treminio <jtreminio@gmail.com>
2016-09-29 19:54:48 -0500

    Remove ssl cert/key location

--- archive/puphpet/puppet/manifests/puphpet/manifests/params.pp
+++ archive/puphpet/puppet/manifests/puphpet/manifests/params.pp
@@ -42,14 +42,4 @@ class puphpet::params {
     xhprof         => hiera_hash('xhprof', {}),
   }
 
-  $ssl_cert_location = $::osfamily ? {
-    'Debian' => '/etc/ssl/certs/ssl-cert-snakeoil.pem',
-    'Redhat' => '/etc/ssl/certs/ssl-cert-snakeoil'
-  }
-
-  $ssl_key_location = $::osfamily ? {
-    'Debian' => '/etc/ssl/private/ssl-cert-snakeoil.key',
-    'Redhat' => '/etc/ssl/certs/ssl-cert-snakeoil'
-  }
-
 }
